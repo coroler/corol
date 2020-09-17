9b1e825dcadf862738288941c4a922f07cbc60dc
coroler <43022925+coroler@users.noreply.github.com>
2020-09-17 21:23:28 +0800

    Update sodoku.py

--- sodoku.py
+++ sodoku.py
@@ -1,7 +1,7 @@
 import copy
 import sys
 
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10000)
 
 board = [[".", "7", "3", ".", "9", ".", ".", ".", "6"],
          [".", ".", ".", ".", "2", ".", ".", "4", "."],
@@ -14,7 +14,7 @@ board = [[".", "7", "3", ".", "9", ".", ".", ".", "6"],
          ["6", ".", ".", ".", ".", ".", ".", ".", "."]]
 
 
-class Solution():
+class Solution(object):
     def __init__(self):
         self.row = 0
         self.cloumn = 0
@@ -22,18 +22,18 @@ class Solution():
         self.insert_flg_list = [[False for _ in range(9)] for _ in range(9)]
         self.count = 0
 
-    def get_check_list(self, tmp_board):
-        row_list = [num for num in tmp_board[self.row] if num != '.']
-        cloumn_list = [nums[self.cloumn] for nums in tmp_board if nums[self.cloumn] != '.']
+    def get_check_list(self, board):
+        row_list = [num for num in board[self.row] if num != '.']
+        cloumn_list = [nums[self.cloumn] for nums in board if nums[self.cloumn] != '.']
         row_num = self.row / 3
         cloumn_num = self.cloumn / 3
         square_list = list()
         for i in range(3):
             for j in range(3):
-                if tmp_board[3 * row_num + i][3 * cloumn_num + j] != '.':
-                    square_list.append(tmp_board[3 * row_num + i][3 * cloumn_num + j])
+                if board[3 * row_num + i][3 * cloumn_num + j] != '.':
+                    square_list.append(board[3 * row_num + i][3 * cloumn_num + j])
 
-        num = tmp_board[self.row][self.cloumn]
+        num = board[self.row][self.cloumn]
         if num in row_list:
             row_list.remove(num)
         if num in cloumn_list:
@@ -45,38 +45,25 @@ class Solution():
     
     def insert(self, board, tmp_board):
         self.count += 1
-        import pdb
-        pdb.set_trace()
         if self.complete_flg:
             return
 
         insert_flg = False
 
-        if self.count >= 22760:
-            for i in range(9):
-                print tmp_board[i]
+        row_list, cloumn_list, square_list = self.get_check_list(board)
 
-        row_list, cloumn_list, square_list = self.get_check_list(tmp_board)
-        if self.count >= 22760:
-            for i in range(9):
-                print "=============================================="
-                print row_list
-                print cloumn_list
-                print square_list
-                print "=============================================="
-
-        if board[self.row][self.cloumn] == '.':
-            if tmp_board[self.row][self.cloumn] == '.':
+        if tmp_board[self.row][self.cloumn] == '.':
+            if board[self.row][self.cloumn] == '.':
                 num = 0
             else:
-                num = int(tmp_board[self.row][self.cloumn])
+                num = int(board[self.row][self.cloumn])
             while(num < 9):
                 num += 1
                 if str(num) not in row_list and str(num) not in cloumn_list and str(num) not in square_list:
                     insert_flg = True
                     break
             if not insert_flg:
-                tmp_board[self.row][self.cloumn] = '.'
+                board[self.row][self.cloumn] = '.'
                 while(not self.insert_flg_list[self.row][self.cloumn]):
                     self.cloumn -= 1
                     if self.cloumn == -1:
@@ -92,10 +79,10 @@ class Solution():
                 self.cloumn = 0
             if self.row == 9:
                 self.complete_flg = True
-            self.insert(board, tmp_board)
+            return self.insert(board, tmp_board)
 
         if insert_flg:
-            tmp_board[self.row][self.cloumn] = str(num)
+            board[self.row][self.cloumn] = str(num)
             self.insert_flg_list[self.row][self.cloumn] = True
             self.cloumn += 1
             if self.cloumn == 9:
@@ -105,24 +92,18 @@ class Solution():
                 self.complete_flg = True
 
         if not self.complete_flg:
-            self.insert(board, tmp_board)
-        
-    def solution(self, board):
+            return self.insert(board, tmp_board)
+
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
         tmp_board = copy.deepcopy(board)
         self.insert(board, tmp_board)
-        return tmp_board, self.count
+        return tmp_board
 
 
 if __name__ == '__main__':
-    result, count = Solution().solution(board)
-    for i in range(9):
-        print board[i]
-
-    print "==============================================="
-
-    for i in range(9):
-        print result[i]
-
-    print "==============================================="
-    
-    print count
+    result = Solution().solveSudoku(board)
+    print result
